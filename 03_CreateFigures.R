a5526372eff8193dd5fac39810b66a1e6622adba
Martin-Jung <martinjung@zoho.com>
2023-02-28 23:54:52 +0100

    Update of figures and code

--- 03_CreateFigures.R
+++ 03_CreateFigures.R
@@ -10,7 +10,7 @@ library(lme4)
 library(brms)
 library(tmap)
 # Default theme and design
-mytheme <- theme_gray(base_size = 20) +
+mytheme <- theme_bw(base_size = 20) +
   theme(
     panel.background = element_rect(#fill = "transparent",
                                     colour = NA_character_), # necessary to avoid drawing panel outline
@@ -53,8 +53,8 @@ table(df$Planning.purpose); table(df$Planning.purpose) / nrow(df)
 table(df$Method); table(df$Method) / nrow(df)
 
 # Connectivity
-table(df$Connectivity); table(df$Connectivity) / nrow(df)
-0.154 + 0.0553 + 0.0197
+table(df$Connectivity); prop <- table(df$Connectivity) / nrow(df)
+sum(prop[-1])
 
 # Current or future
 table(df$Period); table(df$Period) / nrow(df)
@@ -99,8 +99,10 @@ ggplot(o, aes(x = Year, y = N, colour = Extent)) +
   scale_y_continuous(breaks = pretty_breaks(10)) +
   scale_colour_aaas() +
   theme(legend.position = c(.2,.75)) +
+  theme(legend.title = element_text(size = 27), legend.text = element_text(size = 25),
+        axis.title = element_text(size = 26) ,axis.text = element_text(size = 26)) +
   labs(y = "Number of studies", x = "")
-ggsave(filename = "figures/exploratory_timeseries.png", width = 8, height = 8, dpi = 400)
+ggsave(filename = "figures/exploratory_timeseries.png", width = 10, height = 9, dpi = 400)
 
 # Realm
 o <- df |> group_by(Realm) |> summarise(N = n())
@@ -110,8 +112,10 @@ ggplot(o, aes(x = Realm, y = N)) +
   mytheme +
   geom_bar(stat = "identity", fill = "grey20",show.legend = FALSE) +
   scale_y_continuous(expand = c(0,Inf)) +
+  theme(legend.title = element_text(size = 27), legend.text = element_text(size = 25),
+        axis.title = element_text(size = 26) ,axis.text = element_text(size = 26)) +
   labs(x = "", y = "Number of studies")
-ggsave(filename = "figures/exploratory_realms.png", width = 7, height = 6, dpi = 400)
+ggsave(filename = "figures/exploratory_realms.png", width = 10, height = 9, dpi = 400)
 
 # --- #
 # For each planning purpose, summarize the latter columns and
@@ -148,7 +152,7 @@ ggplot(o, aes(x = var1, y = var2, size = n, colour = prop)) +
   mytheme +
   geom_point(stroke = 2,colour="black",show.legend = FALSE) +
   geom_point(stroke = 1) +
-  scale_size_area(guide = guide_legend("Number of studies",override.aes = list(colour="white"))) +
+  scale_size_area(guide = guide_legend("Number of studies",override.aes = list(colour="black"))) +
   scale_colour_gradientn(colours = viridis::inferno(50),
                          guide = guide_colorbar("Proportion", barwidth = unit(3,'in'), title.vjust = .75)) +
   theme(legend.position = "bottom", legend.box = "horizontal",legend.box.background = element_rect(color = NA)) +
@@ -158,7 +162,7 @@ ggplot(o, aes(x = var1, y = var2, size = n, colour = prop)) +
   theme(axis.text.x.bottom = element_text(hjust = 1, angle = 45)) +
   # Reduce distance between y-axis
   theme(plot.title = element_text(hjust = 0.5),
-        legend.text = element_text(color = "white"), legend.title = element_text(color = "white") )
+        legend.text = element_text(color = "black"), legend.title = element_text(color = "black") )
 ggsave("figures/exploratory_bubble.png",width = 12,height = 10,dpi = 400)
 
 #### Create spatial map of density of studies ####
@@ -167,10 +171,14 @@ ggsave("figures/exploratory_bubble.png",width = 12,height = 10,dpi = 400)
 library(raster)
 library(RStoolbox)
 library(sf)
-template <- raster("../../BIOCLIMA/data/referenceraster_10000.tif")
-# template <- raster("../../BIOCLIMA/bioclima_code/data/referenceraster_10000.tif")
+# Load BIOCLIMA raster
+if(Sys.info()["sysname"] == "Windows"){
+  template <- raster("../../BIOCLIMA/bioclima_code/data/referenceraster_10000.tif")
+} else {
+  template <- raster("../../BIOCLIMA/data/referenceraster_10000.tif")
+}
 
-terrestrial <- st_read("extdata/TerrestrialRegions.gpkg") |> st_transform(crs = st_crs(template)) |>
+terrestrial <- st_read("extdata/TerrestrialRegions.gpkg") |> sf::st_transform(crs = st_crs(template)) |>
   filter(!SOVEREIGNT %in% c("Iceland", "Moldova"))
 marine <- st_read("extdata/MarineRegions.gpkg") |> st_transform(crs = st_crs(template)) |>
   filter(!(GEONAME %in% c("Jan Mayen Exclusive Economic Zone","Joint regime area Iceland / Norway (Jan Mayen)",
@@ -178,6 +186,8 @@ marine <- st_read("extdata/MarineRegions.gpkg") |> st_transform(crs = st_crs(tem
                           "Greenlandic Exclusive Economic Zone")))
 
 df$Region[df$Region=="Mediteranean"] <- "Mediterranean"
+assertthat::assert_that(!anyNA(df$Region))
+
 out <- raster::stack()
 for(id in 1:nrow(df)){
   print(id)
@@ -231,7 +241,7 @@ raster::nlayers(out)
 
 ras <- sum(out, na.rm = TRUE)
 ras[ras==0] <- NA
-plot(ras, col = ibis.iSDM:::ibis_colours$sdm_colour )
+# plot(ras, col = ibis.iSDM:::ibis_colours$sdm_colour )
 
 # m <- raster::mask(ras, template, inverse = TRUE)
 
@@ -239,21 +249,22 @@ plot(ras, col = ibis.iSDM:::ibis_colours$sdm_colour )
 ter <- terrestrial |> st_crop(extent(ras))
 
 ggR(ras, geom_raster=TRUE) +
-  mytheme + theme(panel.background = element_blank()) +
+  mytheme + theme(panel.background = element_blank(),plot.background = element_blank()) +
+  theme(panel.grid.major = element_line(colour = "grey60")) + # Increase visibility of grid lines
   scale_fill_gradientn(colors = scico(100, palette = "hawaii", direction = -1), na.value = NA,
                        guide = guide_colourbar(title = "Number of studies",barwidth = unit(3, 'in'),
-                                               label.theme = element_text(color = "white",size = 20),
-                                               title.theme = element_text(color = "white",size = 19)  ) ) +
+                                               label.theme = element_text(color = "black",size = 20),
+                                               title.theme = element_text(color = "black",size = 19)  ) ) +
   geom_sf(data = ter, fill = NA, colour = "grey90", linewidth = .75, show.legend = FALSE) +
   theme(legend.position = "bottom",axis.ticks = element_blank(), axis.text = element_blank()) + labs(x = "", y = "")
-ggsave(plot = last_plot(),"figures/map_densitystudies.png", width = 8, height = 8,dpi = 400)
+ggsave(plot = last_plot(),"figures/map_densitystudies.png", width = 10, height = 8, dpi = 400)
 
 #### Specific plots and hypotheses ####
 # Here we explore specific hypotheses surrounding the collated citation information
 
 # ---- #
 # Idea 1: Are studies of larger study extent more often cited in research and policy contexts
-o <- left_join(df, cit, by = c("newDOI" = "doi")) |> select(Extent,Year, cite_socialmedia:cite_policy)
+o <- left_join(df, cit, by = c("newDOI" = "doi")) |> dplyr::select(Extent,Year, cite_socialmedia:cite_policy)
 
 # Test
 cor.test(o$cite_socialmedia, o$cite_scientific) # Social media does not explain citation rate
@@ -261,7 +272,6 @@ cor.test(o$cite_socialmedia, o$cite_policy) # Also not in policy documents
 cor.test(o$cite_scientific, o$cite_policy) # However more widely cited papers are more likely to be also cited by policy
 
 # Fit zero-inflated bayesian Poisson regressions
-# fit <- glmer(cite_policy~Extent + (1|Year), data = o, family = poisson()) # Scientifically, no difference
 fit1 <- brm(cite_scientific~Extent + (1|Year), data = o, family = zero_inflated_poisson(),backend = "cmdstanr") # Scientifically, no difference
 fit2 <- brm(cite_policy~Extent + (1|Year), data = o, family = zero_inflated_poisson(),backend = "cmdstanr") # Scientifically, no difference
 
@@ -279,13 +289,17 @@ o <- bind_rows(
   me2$Extent |> mutate(type = "Policy documents")
 )
 
+# Save the partial results for later
+saveRDS(o, "resSaves/samples_citationrates.rds")
+o <- readRDS("resSaves/samples_citationrates.rds")
+
 ggplot(o, aes(x = Extent, y = estimate__, ymin = lower__, ymax = upper__)) +
   mytheme +
   geom_pointrange(size = 1.5, linewidth = 1.2) +
   scale_y_continuous(breaks = pretty_breaks(4)) +
   facet_wrap(~type,scales = "free_y") +
   labs(x = "", y = "Relative citation rate")
-ggsave("figures/test_citationrates.png",plot = last_plot(),width = 9,height =4,dpi = 400)
+ggsave("figures/test_citationrates.png",plot = last_plot(),width = 10.5,height = 5,dpi = 400)
 
 # ---- #
 # Idea 2: Are more complex or comprehensive studies more cited?
@@ -296,12 +310,12 @@ o$bin_connect <- ifelse(o$Connectivity == "None", 0, 1) |> factor()
 o$bin_cost <- ifelse(o$Costs == "no", 0, 1) |> factor()
 o$bin_stake <- ifelse(o$Stakeholder.involvement=="no", 0, 1) |> factor()
 
-fit <- glmer(cite_scientific ~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = poisson())
-fit <- glmer(cite_policy ~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = poisson())
-summary(fit)
-plot(effects::allEffects(fit))
+# fit <- glmer(cite_scientific ~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = poisson())
+# fit <- glmer(cite_policy ~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = poisson())
+# summary(fit)
+# plot(effects::allEffects(fit))
 
-# Fit zero-inflated bayesian Poisson regressions
+# Fit zero-inflated Bayesian Poisson regressions
 fit1 <- brm(cite_scientific ~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = zero_inflated_poisson(),backend = "cmdstanr")
 fit2 <- brm(cite_policy~bin_landuse + bin_connect + bin_cost + bin_stake + (1|Year), data = o, family = zero_inflated_poisson(),backend = "cmdstanr")
 
@@ -324,6 +338,11 @@ o <- bind_rows(
   pivot_longer(cols = bin_landuse:bin_stake)
 o$name <- factor(o$name, levels = c("bin_landuse","bin_connect","bin_cost","bin_stake"),
                  labels = c("Land use", "Connectivity", "Costs", "Stakeholders"))
+
+# Save the partial results for later
+saveRDS(o, "resSaves/samples_citationrates_complexity.rds")
+o <- readRDS("resSaves/samples_citationrates_complexity.rds")
+
 o <- o |> filter(value=="1")
 
 ggplot(o, aes(x = name, y = estimate__, ymin = lower__, ymax = upper__)) +
@@ -333,17 +352,60 @@ ggplot(o, aes(x = name, y = estimate__, ymin = lower__, ymax = upper__)) +
   facet_wrap(~type,scales = "free_y") +
   theme(axis.text.x.bottom = element_text(size = 16)) +
   labs(x = "", y = "Relative citation rate")
-ggsave("figures/test_citationrates_complexity.png",plot = last_plot(),width = 10.5,height =4,dpi = 400)
-
+ggsave("figures/test_citationrates_complexity.png",plot = last_plot(),width = 10.5, height = 5,dpi = 400)
 
 # ---- #
 # Idea 2: Are Local and national studies more likely to incorporate stakeholder feedback
-o <- left_join(df, cit, by = c("newDOI" = "doi")) |> select(Policy.relevance, cite_socialmedia:cite_policy)
+o <- left_join(df, cit, by = c("newDOI" = "doi")) |> dplyr::select(Policy.relevance,Stakeholder.involvement,Extent, Year, cite_socialmedia:cite_policy)
 
+# Fit zero-inflated Bayesian Poisson regressions
+fit1 <- brm(Stakeholder.involvement ~ Extent, data = o, family = bernoulli(),backend = "cmdstanr")
+fit2 <- brm(Stakeholder.involvement ~ Policy.relevance, data = o, family = bernoulli(),backend = "cmdstanr")
+
+# Get conditional effects
+me1 <- conditional_effects(fit1,
+                           re_formula = NA, # Include group level effects
+                           prob = 0.95
+)
+me2 <- conditional_effects(fit2,
+                           re_formula = NA, # Include group level effects
+                           prob = 0.95
+)
+
+o <- bind_rows(
+  bind_rows(me1$Extent) |> rename(variable = Extent) |>
+    mutate(type = "Scientific literature"),
+  bind_rows(me2$Policy.relevance) |> rename(variable = Policy.relevance) |>
+    mutate(type = "Policy documents")
+)
+
+# Save the partial results for later
+saveRDS(o, "resSaves/samples_stakeholder.rds")
+o <- readRDS("resSaves/samples_stakeholder.rds")
+
+g1 <- ggplot(o |> filter(type == "Scientific literature"),
+       aes(x = variable, y = estimate__, ymin = lower__, ymax = upper__)) +
+  mytheme +
+  geom_pointrange(size = 1.5, linewidth = 1.2,position = position_dodge(.5)) +
+  scale_y_continuous(breaks = pretty_breaks(4)) +
+  facet_wrap(~type, scales = "free_y",drop = TRUE) +
+  theme(axis.text.x.bottom = element_text(size = 16)) +
+  labs(x = "", y = "Probability of\ninvolvement of stakeholders")
+g2 <- ggplot(o |> filter(type == "Policy documents"),
+             aes(x = variable, y = estimate__, ymin = lower__, ymax = upper__)) +
+  mytheme +
+  geom_pointrange(size = 1.5, linewidth = 1.2,position = position_dodge(.5)) +
+  scale_y_continuous(breaks = pretty_breaks(4)) +
+  facet_wrap(~type, scales = "free_y",drop = TRUE) +
+  theme(axis.text.x.bottom = element_text(size = 16)) +
+  labs(x = "", y = "Probability of\n involvement of stakeholders")
+
+pg <- cowplot::plot_grid(g2, g1 + labs(y=""), align = "h")
+cowplot::ggsave2("figures/test_stakeholderinvolvement.png",plot = pg, width = 10.5, height = 5,dpi = 400)
 
 # ---- #
 # Idea 3: Are studies that are referring to specific policies also more cited in policy agendas?
-o <- left_join(df, cit, by = c("newDOI" = "doi")) |> select(Policy.relevance,Year, cite_socialmedia:cite_policy)
+o <- left_join(df, cit, by = c("newDOI" = "doi")) |> dplyr::select(Policy.relevance,Year, cite_socialmedia:cite_policy)
 
 #fit <- glmer(cite_policy~Policy.relevance + (1|Year), data=o, family = poisson() )
 #plot(effects::allEffects(fit))
@@ -364,26 +426,17 @@ o <- bind_rows(
   me2$Policy.relevance |> mutate(type = "Scientific literature")
 )
 
+# Save the partial results for later
+saveRDS(o, "resSaves/samples_citationrates_policy.rds")
+o <- readRDS("resSaves/samples_citationrates_policy.rds")
+
 ggplot(o, aes(x = Policy.relevance, y = estimate__, ymin = lower__, ymax = upper__)) +
   mytheme +
   geom_pointrange(size = 1.5, linewidth = 1.2) +
   scale_y_continuous(breaks = pretty_breaks(4)) +
   facet_wrap(~type,scales = "free_y") +
   labs(x = "", y = "Relative citation rate")
-ggsave("figures/test_citationrates_policy.png",plot = last_plot(),width = 9.5,height =4,dpi = 400)
+ggsave("figures/test_citationrates_policy.png",plot = last_plot(),width = 10.5,height =5,dpi = 400)
 
 
 # ---- #
-# Idea 4: Does an involvement of stakeholders increase the number of citations (scientific and policy-wise)
-o <- left_join(df, cit, by = c("newDOI" = "doi")) #|> select(Extent,Year,Planning.purpose, Stakeholder.involvement, cite_scientific:cite_policy)
-
-fit <- glmer(cite_scientific ~ Stakeholder.involvement  + (1|Year), data =o, family = poisson())
-summary(fit)
-plot(effects::allEffects(fit))
-
-# ---- #
-# Idea 5: Management actions are usually ecosystem-specific and local scale
-
-o <- df |> filter(Planning.purpose=="Management action")
-
-
